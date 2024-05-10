3e8df808a8b93afec043d2b98bd4405889f1e05b
rosoa0475 <rosoa0475@gmail.com>
2024-05-10 14:16:14 +0900

    [FEAT] 프로젝트 시작

---
+++ src/test/java/com/fourback/bemajor/BemajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourback.bemajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemajorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
