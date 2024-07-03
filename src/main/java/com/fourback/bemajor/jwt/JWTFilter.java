09f41ac9109e745e25489647224648c2539f20b4
rosoa0475 <rosoa0475@gmail.com>
2024-07-04 02:21:53 +0900

    [FIX] handle exception in filter

--- src/main/java/com/fourback/bemajor/jwt/JWTFilter.java
+++ src/main/java/com/fourback/bemajor/jwt/JWTFilter.java
@@ -4,10 +4,12 @@ import com.fourback.bemajor.domain.CustomUserDetails;
 import com.fourback.bemajor.dto.UserAuthDto;
 import com.fourback.bemajor.exception.AccessTokenExpiredException;
 import com.fourback.bemajor.exception.InvalidLoginTokenException;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,24 +18,43 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JWTFilter extends OncePerRequestFilter
-{
+@Slf4j
+public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
 
-    public JWTFilter(JWTUtil jwtUtil){
+    public JWTFilter(JWTUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = request.getHeader("access");
-        if(accessToken == null){
-            filterChain.doFilter(request, response);
+        String path = request.getContextPath();
+        String method = request.getMethod();
+        if (accessToken == null) {
+            if (isNotPostAuth(path, method)) {
+                filterChain.doFilter(request, response);
+            } else {
+                log.debug("access token empty");
+                throw new InvalidLoginTokenException(4, "This is Invalid Token. Try logging in again", HttpStatus.UNAUTHORIZED);
+            }
             return;
         }
-        if (jwtUtil.isExpired(accessToken)) {
-            //response status code
-            throw new AccessTokenExpiredException(6, "Access Token Expired", HttpStatus.UNAUTHORIZED);
+        try {
+            jwtUtil.isExpired(accessToken);
+        } catch (ExpiredJwtException e) {
+            if(isNotPostAuth(path, method)) {
+                throw new AccessTokenExpiredException(6, "Access Token Expired", HttpStatus.UNAUTHORIZED);
+            }
+            else{
+                filterChain.doFilter(request, response);
+            }
+        }
+
+        String category = jwtUtil.getCategory(accessToken);
+        if(!category.equals("access")) {
+            log.debug("invalid access token");
+            throw new InvalidLoginTokenException(4, "This is Invalid Token. Try logging in again", HttpStatus.UNAUTHORIZED);
         }
 
         String username = jwtUtil.getUsername(accessToken);
@@ -43,5 +64,9 @@ public class JWTFilter extends OncePerRequestFilter
         Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null, customOAuth2User.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authToken);
         filterChain.doFilter(request, response);
+    }
+
+    private boolean isNotPostAuth(String path, String method){
+        return !path.equals("/auth") || !method.equals("POST");
     }
 }
