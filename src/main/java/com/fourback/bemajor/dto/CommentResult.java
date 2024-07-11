0b9918f71f4ebf47583dabecdd8b8870d6aa3fba
DESKTOP-ME5QO6B\admin <wlwlwlal2@gmail.com>
2024-07-11 17:21:46 +0900

    update comment

--- src/main/java/com/fourback/bemajor/dto/CommentResult.java
+++ src/main/java/com/fourback/bemajor/dto/CommentResult.java
@@ -2,6 +2,7 @@ package com.fourback.bemajor.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fourback.bemajor.domain.CommentType;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -32,11 +33,17 @@ public class CommentResult {
     private GetCommentListResponse reply;
 
     public static CommentResult fromComment(Comment comment) {
+        String commentContent = "";
+        if(comment.getStatus() == CommentType.DEFAULT) {
+            commentContent = comment.getContent();
+        } else if(comment.getStatus() == CommentType.DELETED) {
+            commentContent = "삭제된 댓글입니다.";
+        }
         return CommentResult.builder()
                 .id(comment.getId())
                 .userName(comment.getUser().getUserName())
-                .content(comment.getContent())
                 .goodCount(comment.getGoodCount())
+                .content(commentContent)
                 .commentDate(comment.getCreatedDate())
                 .build();
     }
