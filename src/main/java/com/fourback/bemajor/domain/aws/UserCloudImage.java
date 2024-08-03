9cf37073b135519113f4ab6452795c47ee462f79
devchlee12 <devchlee12@gmail.com>
2024-08-03 20:01:07 +0900

    [FEAT] 유저 S3 이미지 엔티티 생성

---
+++ src/main/java/com/fourback/bemajor/domain/aws/UserCloudImage.java
@@ -0,0 +1,34 @@
+package com.fourback.bemajor.domain.aws;
+
+import com.fourback.bemajor.domain.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 유저 클라우드 이미지 엔티티
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class UserCloudImage {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String imagePath;
+  @OneToOne(fetch = FetchType.LAZY)
+  @Setter
+  private User user;
+
+  public UserCloudImage(Long imageId, String imagePath) {
+    this.id = imageId;
+    this.imagePath = imagePath;
+  }
+}
