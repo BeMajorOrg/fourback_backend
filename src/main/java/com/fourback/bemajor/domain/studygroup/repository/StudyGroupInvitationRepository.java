2ad10626205b404da872a35fa94baf60508fb230
devchlee12 <devchlee12@gmail.com>
2024-08-25 15:50:47 +0900

    [FEAT] 스터디 그룹 초대 리포지토리 추가

---
+++ src/main/java/com/fourback/bemajor/domain/studygroup/repository/StudyGroupInvitationRepository.java
@@ -0,0 +1,16 @@
+package com.fourback.bemajor.domain.studygroup.repository;
+
+import com.fourback.bemajor.domain.studygroup.entity.StudyGroupInvitation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * 스터디 그룹 초대 리포지토리
+ */
+public interface StudyGroupInvitationRepository extends JpaRepository<StudyGroupInvitation, Long> {
+  Integer countAllByUser_UserId(Long id);
+
+  List<StudyGroupInvitation> findAllByUser_UserId(Long id);
+}
