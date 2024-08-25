cebc094c789ce80f09702493a3a222bc4768f828
devchlee12 <devchlee12@gmail.com>
2024-08-25 15:49:44 +0900

    [REFACTOR] 코드 정리

--- src/main/java/com/fourback/bemajor/domain/user/entity/UserEntity.java
+++ src/main/java/com/fourback/bemajor/domain/user/entity/UserEntity.java
@@ -3,11 +3,23 @@ package com.fourback.bemajor.domain.user.entity;
 import com.fourback.bemajor.domain.studygroup.entity.StudyJoined;
 import com.fourback.bemajor.domain.user.dto.request.UserUpdateRequestDto;
 import com.fourback.bemajor.domain.user.dto.response.UserResponseDto;
-import jakarta.persistence.*;
-import lombok.*;
+
 import org.hibernate.annotations.SQLDelete;
 
 import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -17,74 +29,80 @@ import java.util.List;
 @Builder
 @Table(name = "user")
 public class UserEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    Long userId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  Long userId;
 
-    @Column(name = "name")
-    String userName;
+  @Column(name = "name")
+  String userName;
 
-    @Column(name = "email")
-    String email;
+  @Column(name = "email")
+  String email;
 
-    @Column(name = "birth")
-    String birth;
+  @Column(name = "birth")
+  String birth;
 
-    @Column(name = "oauth2_id")
-    String oauth2Id;
+  @Column(name = "oauth2_id")
+  String oauth2Id;
 
-    @Column(name = "role")
-    String role;
+  @Column(name = "role")
+  String role;
 
-    @Column(name = "belong")
-    String belong;
+  @Column(name = "belong")
+  String belong;
 
-    @Column(name = "department")
-    String department;
+  @Column(name = "department")
+  String department;
 
-    @Column(name = "bobby")
-    String hobby;
+  @Column(name = "bobby")
+  String hobby;
 
-    @Column(name = "objective")
-    String objective; //희망 분야
+  @Column(name = "objective")
+  String objective; //희망 분야
 
-    @Column(name = "address")
-    String address;
+  @Column(name = "address")
+  String address;
 
-    @Column(name = "tech_stack")
-    String techStack;
+  @Column(name = "tech_stack")
+  String techStack;
 
-    @Setter
-    @Column(name = "is_deleted")
-    boolean isDeleted;
+  @Setter
+  @Column(name = "is_deleted")
+  boolean isDeleted;
 
-    @Setter
-    @Column(name = "user_image")
-    String fileName;
+  @Setter
+  @Column(name = "user_image")
+  String fileName;
 
-    @OneToMany(mappedBy = "user")
-    List<StudyJoined> studyJoineds;
+  @OneToMany(mappedBy = "user")
+  List<StudyJoined> studyJoineds;
 
-    public UserResponseDto toUserResponseDto() {
-        return UserResponseDto.builder()
-                .userName(this.userName).address(this.address)
-                .department(this.department).fileName(this.fileName)
-                .email(this.email).hobby(this.hobby).birth(this.birth)
-                .belong(this.belong).objective(this.objective)
-                .techStack(this.techStack).isDeleted(this.isDeleted)
-                .build();
-    }
+  public UserResponseDto toUserResponseDto() {
+    return UserResponseDto.builder()
+                          .userName(this.userName)
+                          .address(this.address)
+                          .department(this.department)
+                          .fileName(this.fileName)
+                          .email(this.email)
+                          .hobby(this.hobby)
+                          .birth(this.birth)
+                          .belong(this.belong)
+                          .objective(this.objective)
+                          .techStack(this.techStack)
+                          .isDeleted(this.isDeleted)
+                          .build();
+  }
 
-    public void update(UserUpdateRequestDto userUpdateRequestDto) {
-        this.userName = userUpdateRequestDto.getUserName();
-        this.email = userUpdateRequestDto.getEmail();
-        this.birth = userUpdateRequestDto.getBirth();
-        this.belong = userUpdateRequestDto.getBelong();
-        this.department = userUpdateRequestDto.getDepartment();
-        this.hobby = userUpdateRequestDto.getHobby();
-        this.objective = userUpdateRequestDto.getObjective();
-        this.address = userUpdateRequestDto.getAddress();
-        this.techStack = userUpdateRequestDto.getTechStack();
-    }
+  public void update(UserUpdateRequestDto userUpdateRequestDto) {
+    this.userName = userUpdateRequestDto.getUserName();
+    this.email = userUpdateRequestDto.getEmail();
+    this.birth = userUpdateRequestDto.getBirth();
+    this.belong = userUpdateRequestDto.getBelong();
+    this.department = userUpdateRequestDto.getDepartment();
+    this.hobby = userUpdateRequestDto.getHobby();
+    this.objective = userUpdateRequestDto.getObjective();
+    this.address = userUpdateRequestDto.getAddress();
+    this.techStack = userUpdateRequestDto.getTechStack();
+  }
 }
