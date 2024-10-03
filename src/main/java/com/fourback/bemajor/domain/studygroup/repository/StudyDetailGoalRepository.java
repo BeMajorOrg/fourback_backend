9216ab973023e5e3a9fd1e42c22d89e117daa1c9
Chanho lee <devchlee12@gmail.com>
2024-10-03 23:58:06 +0900

    [FEAT] 스터디 세부 목표 리포지토리 추가

---
+++ src/main/java/com/fourback/bemajor/domain/studygroup/repository/StudyDetailGoalRepository.java
@@ -0,0 +1,7 @@
+package com.fourback.bemajor.domain.studygroup.repository;
+
+import com.fourback.bemajor.domain.studygroup.entity.StudyDetailGoal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyDetailGoalRepository extends JpaRepository<StudyDetailGoal, Long> {
+}
