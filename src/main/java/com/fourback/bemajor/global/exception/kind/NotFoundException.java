bf75a653db2017248819b8396d463449e59633e2
choi <rosoa0475@gmail.com>
2024-10-08 17:30:11 +0900

    [REFACTOR] NotFoundException

--- src/main/java/com/fourback/bemajor/global/exception/kind/NotFoundException.java
+++ src/main/java/com/fourback/bemajor/global/exception/kind/NotFoundException.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
-@Getter
 public class NotFoundException extends CustomException {
     public NotFoundException(String message) {
         super(2, message, HttpStatus.NOT_FOUND);
