5af21e1f5756d14f54da3ec06890381686a4ff7b
choi <rosoa0475@gmail.com>
2024-10-31 11:09:00 +0900

    [FEAT] handle RuntimeException

--- src/main/java/com/fourback/bemajor/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fourback/bemajor/global/exception/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.fourback.bemajor.global.exception;
 
 import com.fourback.bemajor.global.common.util.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.IOException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
@@ -22,6 +23,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<?> handleIOException(IOException ex) {
+        log.error(ex.getMessage(), ex);
         ExceptionDto body = ExceptionDto.of(4, "io error");
 
         return this.handleExceptionInternal(body, HttpStatus.LOCKED);
@@ -29,9 +31,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage(), ex);
         ExceptionDto body = ExceptionDto.of(7, "validation error");
 
         return this.handleExceptionInternal(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
+        ExceptionDto body = ExceptionDto.of(8, "runtime error");
+
+        return this.handleExceptionInternal(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private ResponseEntity<?> handleExceptionInternal(ExceptionDto body, HttpStatusCode statusCode) {
